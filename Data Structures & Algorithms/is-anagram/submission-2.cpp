fc94df373b3f86ac8c575097fce235d9d163a458
Tanya D Shetty <162685320+ShettyTanya@users.noreply.github.com>
2026-09-02 21:52:26 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> seen;
+        for(int i=0;i<s.size();i++){
+            if(!seen.count(s[i])){
+                seen[s[i]]=1;
+            }
+            else{
+                seen[s[i]]+=1;
+            }
+        }
+        for(int i=0;i<t.size();i++){
+            if(!seen.count(t[i])){
+                return false;
+            }
+            else{
+                seen[t[i]]-=1;
+            }
+        }
+        for( auto it:seen){
+            if(it.second!=0){
+                return false;
+            }
+        }
+        return true;
+
+
+        
+    }
+};
